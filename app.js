0e10e730e260010545d8e2cfdb785d41f522b725
FameX2 <70233855+FameX2@users.noreply.github.com>
2023-01-30 20:55:31 +0200

    commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+const createPath = (page) => path.resolve(__dirname, 'pages', `${page}.html`)
+
+app.listen(port, (error => {
+    error ? console.log(error) : console.log(`Connected to port: ${port}`)
+}))
+
+app.use(express.static('styles'))
+
+app.get('/', (req, res) => {
+    res.sendFile(createPath('index'))
+})
+
+app.get('/info', (req, res) => {
+    res.sendFile(createPath('info'))
+})
+
+app.use((req, res) => {
+    res
+    .status(404)
+    .sendFile(createPath('error'))
+})
