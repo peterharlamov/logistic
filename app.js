bd56293fe2125c1bd7230bf88d53aec732cc939d
FameX2 <70233855+FameX2@users.noreply.github.com>
2023-03-30 19:57:09 +0300

    data query fix

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
-const auth = require("/Repositories/logistic/auth")
-// const database = require('D:/Repositories/logistic/database.js')
+const database = require('D:/Repositories/logistic/database.js')
 
 const createPath = (page) => path.resolve(__dirname, 'pages', `${page}.html`)
 
