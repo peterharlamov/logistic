0fbed286cf8f43706a668934988eac052f463d81
FameX2 <70233855+FameX2@users.noreply.github.com>
2023-03-30 19:56:43 +0300

    html fixed

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
-const database = require('D:/Repositories/logistic/database.js')
+const auth = require("/Repositories/logistic/auth")
+// const database = require('D:/Repositories/logistic/database.js')
 
 const createPath = (page) => path.resolve(__dirname, 'pages', `${page}.html`)
 
